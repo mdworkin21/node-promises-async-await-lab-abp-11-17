2a1734b244a6d54ee87c70359e5d81e4ce3fa2f1
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-17 20:53:45 +0000

    automatically backed up by learn

--- models/Question.js
+++ models/Question.js
@@ -27,9 +27,9 @@ class Question {
   }
 
   // FIXME: Implement Promise
-  static async Find(id){
+  static Find(id){
     console.log(`Waiting for Question ${id} to load...`)
-    await return new Promise(function(resolve){
+     return new Promise(function(resolve){
       setTimeout(function(){
         const question = Question._All[id-1] // Do Not Edit
         console.log(`...Question ${id} Loaded`) // Do Not Edit
